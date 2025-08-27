6309b8798badd4e92bae16538b073dc6b1b79bc2
Pekka Nikander <pekka.nikander@iki.fi>
2025-08-27 15:21:00 +0300

    FAILED: PureScript implementation - LLM incompetence with PureScript syntax and type system

--- purescript/src/Types.purs
+++ purescript/src/Types.purs
@@ -20,3 +20,96 @@ businessId (Business _ _ _ bi _) = bi
 
 form :: Business -> String
 form (Business _ _ _ _ f) = f
+
+-- Account types in the Finnish accounting system
+-- These represent the chart of accounts structure
+data AccountType
+  -- Assets (Vastaavaa)
+  = ASS           -- General assets
+  | ASS_DEP       -- Depreciable assets (Poistokelpoinen omaisuus)
+  | ASS_VAT       -- VAT receivables (Arvonlisäverosaatava)
+  | ASS_REC       -- Trade receivables (Siirtosaamiset)
+  | ASS_PAY       -- Bank account / Cash (Pankkitili / käteisvarat)
+  | ASS_DUE       -- Sales receivables (Myyntisaatavat)
+
+  -- Liabilities (Vastattavaa)
+  | LIA           -- General liabilities
+  | LIA_EQU       -- Equity (Oma pääoma)
+  | LIA_PRE       -- Previous periods' profit (Edellisten tilikausien voitto)
+  | LIA_DUE       -- Trade payables (Ostovelat)
+  | LIA_DEB       -- Debts (Velat)
+  | LIA_ACC       -- Accrued liabilities (Siirtovelat)
+  | LIA_VAT       -- VAT liability (Arvonlisäverovelka)
+
+  -- Revenue (Tulot)
+  | REV           -- General revenue
+  | REV_SAL       -- Sales revenue (Liikevaihtotulot / myynti)
+  | REV_NO        -- Tax-free revenue (Verottomat tulot)
+
+  -- Expenses (Menot)
+  | EXP           -- General expenses
+  | EXP_DEP       -- Depreciation (Poistot)
+  | EXP_NO        -- Non-deductible expenses (Vähennyskelvottomat menot)
+  | EXP_50        -- Half-deductible expenses (Puoliksi vähennyskelpoiset menot)
+  | EXP_TAX       -- Tax account (Verotili)
+  | EXP_TAX_PRE   -- Prepaid taxes (Ennakkoverot)
+
+-- Main Account interface representing a chart of accounts entry
+data Account = Account
+  Int           -- id
+  String        -- number (Account number e.g., "1000")
+  Int           -- padded_number (Numeric version for sorting)
+  String        -- name (Account name)
+  AccountType   -- type (Account type from enum)
+  Int           -- default_vat_code
+  Number        -- default_vat_rate
+  String        -- default_vat_rate_label
+  Number        -- opening_balance
+  Number        -- balance (Current balance)
+  Boolean       -- is_shown (Whether to show in UI)
+  Boolean       -- is_used (Whether account has transactions)
+  String        -- created_at (ISO date-time string)
+  String        -- updated_at (ISO date-time string)
+
+-- Account field accessors
+accountId :: Account -> Int
+accountId (Account i _ _ _ _ _ _ _ _ _ _ _ _) = i
+
+accountNumber :: Account -> String
+accountNumber (Account _ n _ _ _ _ _ _ _ _ _ _ _) = n
+
+accountPaddedNumber :: Account -> Int
+accountPaddedNumber (Account _ _ pn _ _ _ _ _ _ _ _ _ _) = pn
+
+accountName :: Account -> String
+accountName (Account _ _ _ nm _ _ _ _ _ _ _ _ _) = nm
+
+accountType :: Account -> AccountType
+accountType (Account _ _ _ _ t _ _ _ _ _ _ _ _) = t
+
+accountDefaultVatCode :: Account -> Int
+accountDefaultVatCode (Account _ _ _ _ _ vc _ _ _ _ _ _ _) = vc
+
+accountDefaultVatRate :: Account -> Number
+accountDefaultVatRate (Account _ _ _ _ _ _ vr _ _ _ _ _ _) = vr
+
+accountDefaultVatRateLabel :: Account -> String
+accountDefaultVatRateLabel (Account _ _ _ _ _ vl _ _ _ _ _ _) = vl
+
+accountOpeningBalance :: Account -> Number
+accountOpeningBalance (Account _ _ _ _ _ _ _ ob _ _ _ _ _) = ob
+
+accountBalance :: Account -> Number
+accountBalance (Account _ _ _ _ _ _ _ _ b _ _ _ _) = b
+
+accountIsShown :: Account -> Boolean
+accountIsShown (Account _ _ _ _ _ _ _ _ _ is _ _ _) = is
+
+accountIsUsed :: Account -> Boolean
+accountIsUsed (Account _ _ _ _ _ _ _ _ _ _ iu _ _) = iu
+
+accountCreatedAt :: Account -> String
+accountCreatedAt (Account _ _ _ _ _ _ _ _ _ _ _ ca _) = ca
+
+accountUpdatedAt :: Account -> String
+accountUpdatedAt (Account _ _ _ _ _ _ _ _ _ _ _ _ _ ua) = ua
--- purescript/test/Main.purs
+++ purescript/test/Main.purs
@@ -24,6 +24,20 @@ main = runSpecAndExitProcess [ consoleReporter ] do
         T.businessId business `shouldEqual` "1234567-8"
         T.form business `shouldEqual` "FI_YHD"
 
+      it "should create an Account with all required fields" do
+        let account = T.Account 1 "1000" 1000 "Bank Account" T.ASS_PAY 1 25.5 "standard" 0.0 1000.0 true true "2024-01-01" "2024-01-01"
+        T.accountId account `shouldEqual` 1
+        T.accountNumber account `shouldEqual` "1000"
+        T.accountName account `shouldEqual` "Bank Account"
+        T.accountType account `shouldEqual` T.ASS_PAY
+        T.accountBalance account `shouldEqual` 1000.0
+
+      it "should validate AccountType enum values" do
+        T.ASS `shouldEqual` T.ASS
+        T.LIA `shouldEqual` T.LIA
+        T.REV `shouldEqual` T.REV
+        T.EXP `shouldEqual` T.EXP
+
     describe "Accounting" do
       it "should calculate total assets correctly" do
         true `shouldEqual` true
