b1acc8ade657608450e9f59f3b53b1a85604bc3d
Pekka Nikander <pekka.nikander@iki.fi>
2025-08-27 13:03:28 +0200

    Add failing Business type test - TDD Red phase

--- purescript/test/Main.purs
+++ purescript/test/Main.purs
@@ -14,9 +14,19 @@ main = runSpecAndExitProcess [ consoleReporter ] do
     describe "Types" do
       it "should have valid account type constructors" do
         true `shouldEqual` true
+
+      it "should create a Business with all required fields" do
+        let business = Business 1 "holotropic" "Holotropic Oy" "1234567-8" "FI_YHD"
+        business.id `shouldEqual` 1
+        business.slug `shouldEqual` "holotropic"
+        business.name `shouldEqual` "Holotropic Oy"
+        business.businessId `shouldEqual` "1234567-8"
+        business.form `shouldEqual` "FI_YHD"
+
     describe "Accounting" do
       it "should calculate total assets correctly" do
         true `shouldEqual` true
+
     describe "Streams" do
       it "should filter accounts by type" do
         true `shouldEqual` true
