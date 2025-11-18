e5915a144b17008b73cc7ec61d9845524779b427
Pekka Nikander <pekka.nikander@iki.fi>
2025-11-18 09:09:26 +0200

    A step towards real tools

--- tools/Program.fs
+++ tools/Program.fs
@@ -1,32 +1,47 @@
 ﻿open System
 open Argu
 
+type NoPrefixAttribute() = inherit CliPrefixAttribute(CliPrefix.None)
+
+type BusinessesArgs =
+    | [< AltCommandLine("-f") >]                  Fields of fields: string list
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Fields _     -> "Comma-separated list of fields to export (default all)."
+
 type AccountsArgs =
-    | [< AltCommandLine("-b"); Mandatory >]  BusinessId of string
-    | [< AltCommandLine("-o") >]             Out of outPath: string
-    | [< AltCommandLine("-f") >]             Fields of fields: string list
+    | [< AltCommandLine("-b"); Mandatory >]       BusinessId of string
+    | [< AltCommandLine("-f") >]                  Fields of fields: string list
     interface IArgParserTemplate with
         member this.Usage =
             match this with
             | BusinessId _ -> "Business identifier (Y-tunnus|VAT-code)."
-            | Out _        -> "Optional CSV output path (default stdout)."
             | Fields _     -> "Comma-separated list of fields to export (default all)."
 
-type ListArgs =
+type EntitiesArgs =
     | [< AltCommandLine("-f") >]                  Format of format: string
-    | [< CliPrefix(CliPrefix.None); SubCommand >] Accounts of ParseResults<AccountsArgs>
+    | [< NoPrefix; SubCommand >]                  Accounts of ParseResults<AccountsArgs>
+    | [< NoPrefix; SubCommand >]                  Businesses of ParseResults<BusinessesArgs>
     interface IArgParserTemplate with
         member this.Usage =
             match this with
             | Format _     -> "Output format (currently only csv)."
-            | Accounts _   -> "List accounts for a resolved business."
+            | Accounts _   -> "Accounts of a business."
+            | Businesses _ -> "Businesses."
 
 type CliArgs =
-    | [<CliPrefix(CliPrefix.None); SubCommand>] List of ParseResults<ListArgs>
+    | [< AltCommandLine("-o") >]                  Out   of outPath: string
+    | [< AltCommandLine("-i") >]                  In    of inPath: string
+    | [< NoPrefix; SubCommand >]                  List  of ParseResults<EntitiesArgs>
+    | [< NoPrefix; SubCommand >]                  Patch of ParseResults<EntitiesArgs>
     interface IArgParserTemplate with
         member this.Usage =
             match this with
+            | Out _        -> "Optional CSV output path (default stdout)."
+            | In _         -> "Optional CSV input path (default stdin)."
             | List _       -> "List entities (businesses, accounts, etc.)."
+            | Patch _      -> "Patch an entity (business, account, etc.)."
 
 [<EntryPoint>]
 let main argv =
